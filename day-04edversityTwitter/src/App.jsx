3a3245231602312f8bddbf601edb3d90a5aecd48
Uzmakh <urbazoha@gmail.com>
2024-01-23 21:21:07 +0500

    Added Routes

--- day-04edversityTwitter/src/App.jsx
+++ day-04edversityTwitter/src/App.jsx
@@ -1,6 +1,7 @@
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Auth from "./pages/Auth";
-// import Setting from "./pages/Setting";
+import Setting from "./pages/Setting";
 import Followers from "./pages/Followers";
 import Followings from "./pages/Followings";
 
@@ -8,10 +9,18 @@ import "./App.css";
 function App() {
    return (
       <>
-         <Followers />
-         {/* <Followings /> */}
-         {/* <Home /> */}
-         {/* <Auth /> */}
+         <Routes>
+            <Route path="/" element={<Auth />} />
+            <Route path=":id" element={<Home />} />
+            <Route path="/home">
+               <Route index element={<Home />} />
+               <Route path="followings" element={<Followings />} />
+               <Route path="followers" element={<Followers />} />
+            </Route>
+      
+      <Route path="/setting" element={<Setting/>} />
+         </Routes>
+
       </>
    );
 }
